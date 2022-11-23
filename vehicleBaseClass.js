4572f2a3cb939b056488f35550d472c03b36badc
AngelTheCreator <angel_guevara94@hotmail.com>
2022-11-22 18:41:50 -0800

    added start and accelerate

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -5,13 +5,29 @@ class Vehicle{
         this.year = year;
         this.color = color;
         this.mileage = mileage;
+        this.passenger = 0;
+        this.started = false;
+        this.numberOfWheels = 0;
     }
 
-    start(){
-        if(this.started){
-            if(this.fuel > 0) {
-                console.log(this.speed += 1);
-            }
+   start(){
+    if(this.fuel > 0){
+        return this.started = true;
+    } else {
+        return this.started = false;
         }
+   }
+   accelerate() {
+    if (this.started) {
+        if (this.fuel > 0) {
+            console.log(this.speed += 1);
+            this.fuel = this.fuel - 1;
+        } else {
+            console.log("out of fuel.");
+            this.stop();
+        }
+    } else {
+        alert("You need to start the engine first.");
     }
+}
 }
