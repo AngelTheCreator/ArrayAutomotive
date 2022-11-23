f2af4d5f4ca7f83408b909eb0c3e93ca21f42e31
AngelTheCreator <angel_guevara94@hotmail.com>
2022-11-22 18:43:39 -0800

    added decelerate, stop, etc plus types of vehicle

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -1,33 +1,86 @@
-class Vehicle{
-    constructor(make, model, year, color, mileage){
+class Vehicle {
+    constructor(make, model, year, color, mileage) {
         this.make = make;
         this.model = model;
         this.year = year;
         this.color = color;
-        this.mileage = mileage;
         this.passenger = 0;
+        this.speed = 0;
+        this.mileage = mileage;
         this.started = false;
         this.numberOfWheels = 0;
     }
 
-   start(){
-    if(this.fuel > 0){
-        return this.started = true;
-    } else {
-        return this.started = false;
-        }
-   }
-   accelerate() {
-    if (this.started) {
+    start() {
         if (this.fuel > 0) {
-            console.log(this.speed += 1);
-            this.fuel = this.fuel - 1;
+            return this.started = true;
+            console.log("engine started...!!!");
         } else {
-            console.log("out of fuel.");
-            this.stop();
+            return this.started = false;
+            console.log("engine cannot start...");
         }
-    } else {
-        alert("You need to start the engine first.");
     }
-}
+    accelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                console.log(this.speed += 1);
+                this.fuel = this.fuel - 1;
+            } else {
+                console.log("out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+    decelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                if (this.speed > 0) {
+                    console.log(this.speed -= 1);
+                    this.fuel = this.fuel - 1;
+                } else {
+                    console.log(this + " has stopped moving");
+                    this.fuel = this.fuel - 1;
+                }
+            } else {
+                console.log("out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+    stop() {
+        this.started = false;
+    }
+
+    drive() {
+        accelerate();
+    }
+    brake() {
+        decelerate();
+    }
+
+    autoPark()
+    {
+
+    }
+
+    autoDrive()
+    {
+      
+    }
+
+    typeOfVehicle(wheels) {
+        if (this.numberOfWheels == 8 && 8 == wheels) {
+            console.log(this.model + " " + this.make + " is a Truck");
+        } else if (this.numberOfWheels == 4 && 4 == wheels) {
+            console.log(this.model + " " + this.make + " is a CAr");
+        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+            console.log(this.model + " " + this.make + " is a Bike");
+        } else {
+            console.log("Unknown type of vehicle");
+        }
+    }
 }
